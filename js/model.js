f58d9ed9da2b6e696f19ae9b8c32dcee67227589
Bjorn Tipling <bjorn@ambientchill.com>
2009-06-09 05:31:35 +0000

    creating a model class that will use sqllite in adobe air and files to persist data

---
+++ js/model.js
@@ -0,0 +1,20 @@
+/*
+  Copyright (c) 2009 Apphacker apphacker@gmail.com
+*/
+
+var model;
+
+if(!model) {
+  model = {};
+}
+
+if ( window.runtime && air && util ) {
+  //requires AIR and util
+
+  model.Model = function ( ) {
+  }
+
+  var _mmp = model.Model.prototype;
+
+}
+
