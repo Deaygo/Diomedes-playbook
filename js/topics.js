f154f9ab6863a5e3c7132a9abdc9c1602f5e8b56
Bjorn Tipling <bjorn@ambientchill.com>
2009-06-13 17:21:35 +0000

    Adding channel, servers, and perform topcis

--- js/topics.js
+++ js/topics.js
@@ -5,17 +5,34 @@
 var topics;
 
 if(!topics) {
+
   topics = {
+
     CHANNELS_CHANGED: "/channels/changed/",
     CHANNEL_SELECTED: "/channel/selected/",
     CHANNEL_ACTIVITY: "/channel/activity/",
+
     USER_ACTIVITY: "/user/activity",
     USER_INPUT: "/user/input/",
     USER_HIGHLIGHT: "/user/highlight/",
+
     PREFS_SAVE: "/prefs/save/",
+
     NETWORK_ADD: "/network/add/",
     NETWORK_EDIT: "/network/edit/",
     NETWORK_DELETE: "/network/delete/",
+
+    SERVER_ADD: "/server/add/",
+    SERVER_EDIT: "/server/edit/",
+    SERVER_DELETE: "/server/delete/",
+
+    CHANNEL_ADD: "/channel/add/",
+    CHANNEL_DELETE: "/channel/delete/",
+
+    PERFORM_ADD: "/perform/add",
+    PERFORM_EDIT: "/perform/edit",
+    PERFORM_DELETE: "/perform/delete",
+
   };
 }
 
