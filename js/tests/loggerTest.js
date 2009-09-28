852e75e96356d558ec8c5bda5ec9db9f76f69102
Bjorn Tipling <bjorn@ambientchill.com>
2009-09-28 01:03:24 +0000

    test infrastructure put in place, now just need to add tests

--- js/tests/loggerTest.js
+++ js/tests/loggerTest.js
@@ -16,8 +16,31 @@ load( 'js/dojo/dojo.js' );
 load( 'js/logger.js' );
 load( 'js/tests/achaean.js' );
 
-var loggingTest = new Achaean( "loggingTest" );
+var log, logTest;
 
-var testLogger = new logger.Logger( "channelName", "serverName" );
+logTest = new Achaean( "loggingTest" );
+log = new logger.Logger( "channelName", "serverName" );
+
+logTest.setUp( function( ) {
+  print( "Running setup" );
+} );
+
+logTest.assert( dojo.isObject( log ), "Log not created" );
+
+logTest.run( function ( ) {
+  var results = logTest.getResults( ), i, errors, error;
+  errors = results.allErrors;
+  for( i = 0; i < errors.length; i++ ) {
+    error = errors[ i ];
+    print( "ERROR: " + error.message );
+    print( " - Type: " + error.assertType );
+    print( " - Required: " + error.required.toString( ) );
+  }
+  if ( results.testFailed ) {
+    throw "Test failed.";
+  } else {
+    print ( "Test did not fail." );
+  }
+} );
 
 print( "***DONE TESTING***" );
