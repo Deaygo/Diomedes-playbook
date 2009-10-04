4adfa16ac7327142a4a8a5160406f1c548192581
Bjorn Tipling <bjorn@ambientchill.com>
2009-10-04 19:05:33 +0000

    Moved channels over to dojo classes and using jslint

--- js/diom/connection/channel.js
+++ js/diom/connection/channel.js
@@ -1,14 +1,20 @@
+/*jslint white: false */
+/*jslint nomen: false */
+/*jslint plusplus: false */
+/*jslint passfail: true */
+/*global window, dojo, util, diom */
 
 dojo.provide( "diom.connection.channel" );
 
-  //Channel Class
-  dConnection.Channel = function ( name, type, server, logPref ) {
+dojo.declare( "diom.connection.Channel", null, {
+
+  constructor: function ( name, type, server, logPref ) {
     this.name = name;
     this.type = type;
     this.server = server;
     this.users = {};
     this.topic = null;
-    this.activityList = new dConnection.ActivityList( );
+    this.activityList = new diom.connection.ActivityList( );
     this.logPref = logPref;
     this.logger = new diom.Logger( server, name );
     this.isLogOpen = false;
@@ -16,12 +22,10 @@ dojo.provide( "diom.connection.channel" );
       this.logger.open( );
       this.isLogOpen = true;
     }
-    util.subscribe( topics.PREFS_CHANGE_LOGGING, this, "handleChangeLoggingPref", [] );
-  }
+    util.subscribe( diom.topics.PREFS_CHANGE_LOGGING, this, "handleChangeLoggingPref", [] );
+  },
 
-  var _clp = dConnection.Channel.prototype;
-
-  _clp.handleChangeLoggingPref = function ( newValue ) {
+  handleChangeLoggingPref: function ( newValue ) {
     this.logPref = newValue;
     if ( newValue && !this.isLogOpen ) {
       this.logger.open( );
@@ -30,53 +34,55 @@ dojo.provide( "diom.connection.channel" );
       this.logger.close( );
       this.isLogOpen = false;
     }
-  }
+  },
 
-  _clp.getChannelName = dConnection.Connection.prototype.getChannelName;
+  getChannelName: diom.connection.getChannelName,
 
-  _clp.setTopic = function ( topic ) {
+  setTopic: function ( topic ) {
     this.topic = topic;
-  }
+  },
 
-  _clp.getTopic = function ( ) {
+  getTopic: function ( ) {
     return this.topic;
-  }
+  },
 
-  _clp.renameUser = function ( oldNick, newNick ) {
+  renameUser: function ( oldNick, newNick ) {
+  	var user;
     if ( oldNick in this.users ) {
-      var user = this.users[ oldNick ];
+      user = this.users[ oldNick ];
       delete this.users[ oldNick ];
       this.users[ newNick ] = user;
     }
-  }
+  },
 
-  _clp.addModes = function ( modes ) {
-    for ( var i = 0; i < modes.length; i++ ) {
-      var mode = modes[i];
-      var nick = mode.arg;
+  addModes: function ( modes ) {
+  	var i, mode, nick, user, channelName;
+    for ( i = 0; i < modes.length; i++ ) {
+      mode = modes[i];
+      nick = mode.arg;
       if ( nick in this.users ) {
-        var user = this.users[ nick ];
-        var channelName = this.getChannelName( this.name );
-        if ( mode.type == "O" ) {
-          if ( mode.toggle == "+" ) {
+        user = this.users[ nick ];
+        channelName = this.getChannelName( this.name );
+        if ( mode.type === "O" ) {
+          if ( mode.toggle === "+" ) {
             user.creator( channelName );
           } else {
             user.deCreator( channelName );
           }
-        } else if ( mode.type == "o" ) {
-          if ( mode.toggle == "+" ) {
+        } else if ( mode.type === "o" ) {
+          if ( mode.toggle === "+" ) {
             user.op( channelName );
           } else {
             user.deOp( channelName );
           }
-        } else if ( mode.type == "v" ) {
-          if ( mode.toggle == "+" ) {
+        } else if ( mode.type === "v" ) {
+          if ( mode.toggle === "+" ) {
             user.voice( channelName );
           } else {
             user.deVoice( channelName );
           }
-        } else if ( mode.type == "h" ) {
-          if ( mode.toggle == "+" ) {
+        } else if ( mode.type === "h" ) {
+          if ( mode.toggle === "+" ) {
             user.halfOp( channelName );
           } else {
             user.deHalfOp( channelName );
@@ -84,71 +90,74 @@ dojo.provide( "diom.connection.channel" );
         }
       }
     }
-  }
+  },
 
-  _clp.setName = function ( name ) {
+  setName: function ( name ) {
     //if it's actually a pm window user can change nicks
     this.name = name;
-  }
+  },
 
-  _clp.getName = function ( ) {
+  getName: function ( ) {
     return this.name;
-  }
+  },
 
-  _clp.remUsers = function ( ) {
-    for ( var nick in this.users ) {
-      delete this.users[nick];
+  remUsers: function ( ) {
+  	var nick;
+    for ( nick in this.users ) {
+			if ( this.users.hasOwnProperty( nick ) ) {
+      	delete this.users[ nick ];
+			}
     }
     this.users = {};
-  }
+  },
 
-  _clp.addUser = function ( user ) {
+  addUser: function ( user ) {
     this.users[ user.nick ] = user;
-  }
+  },
 
-  _clp.hasUser = function ( nick ) {
+  hasUser: function ( nick ) {
     return ( nick in this.users );
-  }
+  },
 
-  _clp.remUser = function ( user ) {
+  remUser: function ( user ) {
     var nick = user.nick;
     if ( nick in this.users ) {
       delete this.users[ user.nick ];
     }
-  }
+  },
 
-  _clp.getUsers = function ( ) {
+  getUsers: function ( ) {
     return this.users;
-  }
+  },
 
-  _clp.addActivity = function ( msg ) {
+  addActivity: function ( msg ) {
     this.activityList.addMessage( msg.clone( ) );
     if ( this.logPref ) {
       this.logger.addLine( msg.getNickWithStatus( this.name ), msg.getMsg( ), msg.datetime ); 
       this.logger.write( );
     }
     this.publishActivity( ( msg.cmd in { "privmsg" : 1, "action" : 1 } ) );
-    delete msg;
-  }
+		msg = null;
+  },
 
-  _clp.clearActivity = function ( ) {
+  clearActivity: function ( ) {
     this.activityList.clearActivity( );
-  }
+  },
 
-  _clp.publishActivity = function ( isPM ) {
-    util.publish( topics.CHANNEL_ACTIVITY, [ this.getChannelName( this.name ), this.server, isPM ] );
-  }
+  publishActivity: function ( isPM ) {
+    util.publish( diom.topics.CHANNEL_ACTIVITY, [ this.getChannelName( this.name ), this.server, isPM ] );
+  },
 
-  _clp.publishUserActivity = function ( ) {
-    util.publish( topics.USER_ACTIVITY, [ this.server, this.getChannelName( this.name ) ] );
-  }
+  publishUserActivity: function ( ) {
+    util.publish( diom.topics.USER_ACTIVITY, [ this.server, this.getChannelName( this.name ) ] );
+  },
 
-  _clp.getActivity = function ( msg ) {
-    var msgs =  this.activityList.getMessages( );
-    return msgs
-  }
+  getActivity: function ( msg ) {
+    var msgs = this.activityList.getMessages( );
+    return msgs;
+  },
 
-  _clp.destroy = function ( ) {
+  destroy: function ( ) {
     this.activityList.destroy( );
     this.logger.close( );
     this.isLogOpen = false;
@@ -157,4 +166,6 @@ dojo.provide( "diom.connection.channel" );
     delete this.activityList;
     delete this.users;
   }
+
+} );
 
