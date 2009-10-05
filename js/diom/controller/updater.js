dc48dcde90d2caebb01e23d3b64f5b91b21d49e8
Bjorn Tipling <bjorn@ambientchill.com>
2009-10-05 00:16:53 +0000

    Updater using dojo classes, passes jslint, air escaped for testing

--- js/diom/controller/updater.js
+++ js/diom/controller/updater.js
@@ -1,46 +1,68 @@
+/*jslint white: false */
+/*jslint nomen: false */
+/*jslint plusplus: false */
+/*jslint passfail: true */
+/*global window, dojo, util, diom, air, runtime */
 
 dojo.provide( "diom.controller.updater" );
 
-  dController.Updater = function ( updateDelay, updateURL ) {
+dojo.declare( "diom.controller.Updater", null, {
+
+  constructor: function ( updateDelay, updateURL ) {
+		var file;
     this.updateURL = updateURL;
-    this.appUpdater = new runtime.air.update.ApplicationUpdaterUI( ); 
+		if ( air ) {
+			this.appUpdater = new runtime.air.update.ApplicationUpdaterUI( ); 
+			file = new air.File( "app:/updateConfig.xml" );
+		} else {
+			this.appUpdater = null;
+			file = null;
+		}
     this.didCheckNow = false;
-    var file = new air.File( "app:/updateConfig.xml" );
-    this.appUpdater.configurationFile = file;
-    this.appUpdater.updateURL = this.updateURL;
-    this.appUpdater.delay = updateDelay;
-    this.appUpdater.addEventListener( air.StatusUpdateEvent.UPDATE_STATUS, util.hitch( this, "onStatus" ) ); 
-    this.appUpdater.initialize();
+		if ( air ) {
+			this.appUpdater.configurationFile = file;
+			this.appUpdater.updateURL = this.updateURL;
+			this.appUpdater.delay = updateDelay;
+    	this.appUpdater.addEventListener( air.StatusUpdateEvent.UPDATE_STATUS, util.hitch( this, "onStatus" ) ); 
+    	this.appUpdater.initialize();
+		}
     util.subscribe( diom.topics.UPDATE_CHECK, this, "checkNow", [] );
     util.subscribe( diom.topics.UPDATE_DELAY_CHANGE, this, "changeUpdateDelay", [] );
     util.subscribe( diom.topics.UPDATE_URL_CHANGE, this, "changeUpdateURL", [] );
-  }
+  },
 
-  var _cupr = dController.Updater.prototype;
-
-  _cupr.onStatus = function ( event ) {
+  onStatus: function ( event ) {
     if ( this.didCheckNow && !event.available ) {
       this.didCheckNow = false;
       util.publish( diom.topics.UPDATE_NO_NEW_UPDATES );
     }
-  }
+  },
 
-  _cupr.changeUpdateURL = function ( updateURL ) {
-    this.appUpdater.delay = updateURL;
-    this.appUpdater.initialize( );
-  }
+  changeUpdateURL: function ( updateURL ) {
+		if ( this.appUpdater ) {
+			this.appUpdater.delay = updateURL;
+			this.appUpdater.initialize( );
+		}
+  },
 
-  _cupr.changeUpdateDelay = function ( updateDelay ) {
-    this.appUpdater.delay = updateDelay;
-    this.appUpdater.initialize( );
-  }
+  changeUpdateDelay: function ( updateDelay ) {
+		if ( this.appUpdater ) {
+			this.appUpdater.delay = updateDelay;
+			this.appUpdater.initialize( );
+		}
+  },
 
-  _cupr.checkNow = function ( ) {
+  checkNow: function ( ) {
     this.didCheckNow = true;
-    this.appUpdater.checkNow( );
+		if ( this.appUpdater ) {
+    	this.appUpdater.checkNow( );
+		}
+  },
+
+  destroy: function ( ) {
+		if ( this.appUpdater ) {
+    	delete this.appUpdater;
+		}
   }
 
-  _cupr.destroy = function ( ) {
-    delete this.appUpdater;
-  }
-
+} );
