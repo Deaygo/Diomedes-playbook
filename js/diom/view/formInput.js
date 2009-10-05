f73149f0cee4745863cf55d7910ac645113919b3
Bjorn Tipling <bjorn@ambientchill.com>
2009-10-05 15:58:17 +0000

    moved formInput over to dojo.create and now passes jslint

--- js/diom/view/formInput.js
+++ js/diom/view/formInput.js
@@ -1,7 +1,15 @@
+/*jslint white: false */
+/*jslint nomen: false */
+/*jslint regexp: false */
+/*jslint plusplus: false */
+/*jslint passfail: true */
+/*global window, dojo, util, diom, air, document, alert */
 
 dojo.provide( "diom.view.formInput" );
 
-  dView.FormInput = function ( node, form ) {
+dojo.declare( "diom.view.FormInput", null, {
+
+  constructor: function ( node, form ) {
     this.MAX_HISTORY_LENGTH = 50;
     this.input = node;
     this.form = form;
@@ -19,114 +27,115 @@ dojo.provide( "diom.view.formInput" );
     this.serverName = null;
     this.channels = [];
     util.subscribe( diom.topics.NICK_CHANGE, this, "handleNickChange", [] );
-  }
+  },
 
-  var _vip = dView.FormInput.prototype;
-
-  _vip.getValue = function ( ) {
+  getValue: function ( ) {
     //TODO: add history here and uparrow behavior
     var value = this.input.value;
     this.setValue( "" );
     this.input.focus( );
     return value;
-  }
+  },
 
-  _vip.setValue = function ( value ) {
-    var input = this.input;
+  setValue: function ( value ) {
+		var length, input;
+    input = this.input;
     input.value = value;
     window.setTimeout( function ( ) {
-      var length = input.value.length;
+      length = input.value.length;
       input.setSelectionRange(length, length);
     }, 0 );
-  }
+  },
 
-  _vip.focus = function ( ) {
+  focus: function ( ) {
     this.input.focus( );
-  }
+  },
 
-  _vip.handleInput = function ( e ) {
+  handleInput: function ( e ) {
+		var _input, inputs, i, input;
     util.stopEvent( e );
-    var _input = this.getValue( );
-    var inputs = _input.split( "\n" );
-    for ( var i = 0; i < inputs.length; i++ ) {
-      var input = inputs[ i ];
+    _input = this.getValue( );
+    inputs = _input.split( "\n" );
+    for ( i = 0; i < inputs.length; i++ ) {
+      input = inputs[ i ];
       this.addToHistory( input );
       util.log("getInput: " + input );
       util.publish( diom.topics.USER_INPUT, [ input ] );
     }
-  }
+  },
 
-  _vip.addToHistory = function ( input ) {
+  addToHistory: function ( input ) {
     this.history.unshift( input );
     if ( this.history.length > this.MAX_HISTORY_LENGTH ) {
       this.history.pop( );
     }
-  }
+  },
 
-  _vip.changeChannel = function ( nicks, serverName, channelName ) {
+  changeChannel: function ( nicks, serverName, channelName ) {
     this.serverName = serverName;
     this.channelName = channelName;
     this.nicks = nicks;
-  }
+  },
 
-  _vip.setChannels = function ( channels ) {
+  setChannels: function ( channels ) {
     this.channels = channels;
-  }
+  },
 
-  _vip.handleNickChange = function ( nicks, serverName, channelName ) {
-    if ( serverName == this.serverName && channelName == this.channelName ) {
+  handleNickChange: function ( nicks, serverName, channelName ) {
+    if ( serverName === this.serverName && channelName === this.channelName ) {
       this.nicks = nicks;
     }
-  }
+  },
 
-  _vip.handleInputChange = function ( e ) {
+  handleInputChange: function ( e ) {
     // window.runtime.flash.display
-    var key = e.keyCode;
-    if ( key == 9 ) {
+		var key, index;
+    key = e.keyCode;
+    if ( key === 9 ) {
       //tab
       this.tabCompletion( e );
       return;
-    } else if ( key == 78 && ( e.metaKey || e.ctrlKey ) ) {
+    } else if ( key === 78 && ( e.metaKey || e.ctrlKey ) ) {
       //cntrl+n or command key+n
       util.stopEvent( e );
       util.publish( diom.topics.INPUT_CHANNEL_NEXT );
       return;
-    } else if ( key == 39 && e.shiftKey && ( e.metaKey || e.ctrlKey ) ) {
+    } else if ( key === 39 && e.shiftKey && ( e.metaKey || e.ctrlKey ) ) {
       //cntrl+shift+r arrow or command key+shift+r arrow
       util.stopEvent( e );
       util.publish( diom.topics.INPUT_CHANNEL_NEXT );
       return;
-    } else if ( key == 80 && ( e.metaKey || e.ctrlKey ) ) {
+    } else if ( key === 80 && ( e.metaKey || e.ctrlKey ) ) {
       //cntrl+p or command key+p
       util.stopEvent( e );
       util.publish( diom.topics.INPUT_CHANNEL_PREV );
       return;
-    } else if ( key == 37 && e.shiftKey && ( e.metaKey || e.ctrlKey ) ) {
+    } else if ( key === 37 && e.shiftKey && ( e.metaKey || e.ctrlKey ) ) {
       //cntrl+shift+l arrow or command key+shift+l arrow
       util.stopEvent( e );
       util.publish( diom.topics.INPUT_CHANNEL_PREV );
       return;
-    } else if ( key == 76 && ( e.metaKey || e.ctrlKey ) ) {
+    } else if ( key === 76 && ( e.metaKey || e.ctrlKey ) ) {
       util.stopEvent( e );
       util.publish( diom.topics.INPUT_CHANNEL_PART );
       return;
-    } else if ( key == 13 ) {
+    } else if ( key === 13 ) {
       //enter
       this.handleInput ( e );
       return;
-    } else if ( key == 38 ) {
+    } else if ( key === 38 ) {
       //up arrow
       this.handleHistoryUp( );
       return;
-    } else if ( key == 40 ) {
+    } else if ( key === 40 ) {
       //down arrow
       this.handleHistoryDown( );
       return;
-    } else if ( key == 33 ) {
+    } else if ( key === 33 ) {
       //page up
       util.publish( diom.topics.INPUT_PAGE_UP );
       return;
-    } else if ( key == 34 ) {
+    } else if ( key === 34 ) {
       //page down
       util.publish( diom.topics.INPUT_PAGE_DOWN );
       return;
@@ -136,17 +145,18 @@ dojo.provide( "diom.view.formInput" );
     if ( e.metaKey || e.ctrlKey ) {
       if ( key > 46 && key < 59 ) {
         util.stopEvent( e );
-        var index = key - 49;
-        if ( index < 0 ) index = 9;
+        index = key - 49;
+        if ( index < 0 ) { index = 9; }
         util.publish( diom.topics.INPUT_CHANNEL_INDEX, [ index ] );
       }
     }
-  }
+  },
 
 
-  _vip.handleHistoryUp = function ( ) {
+  handleHistoryUp: function ( ) {
+		var value;
     this.historyIndex++;
-    var value = this.history[ this.historyIndex - 1 ];
+    value = this.history[ this.historyIndex - 1 ];
     if ( value ) {
       this.setValue( value );
     }
@@ -155,12 +165,13 @@ dojo.provide( "diom.view.formInput" );
       this.setValue( "" );
     }
     this.needsResetting = true;
-  }
+  },
 
-  _vip.handleHistoryDown = function ( ) {
+  handleHistoryDown: function ( ) {
+		var value;
     if ( this.historyIndex ) {
       this.historyIndex--;
-      var value = this.history[ this.historyIndex - 1 ];
+      value = this.history[ this.historyIndex - 1 ];
       if ( value ) {
         this.setValue( value );
         this.needsResetting = true;
@@ -168,9 +179,9 @@ dojo.provide( "diom.view.formInput" );
       } 
     } 
     this.setValue( "" );
-  }
+  },
 
-  _vip.reset = function ( ) {
+  reset: function ( ) {
     //reset whatever tracking code used by fancy
     //key stuff
     if ( this.needsResetting ) {
@@ -182,50 +193,53 @@ dojo.provide( "diom.view.formInput" );
       this.needsResetting = false;
       this.historyIndex = 0;
     }
-  }
+  },
 
-  _vip.tabCompletion = function ( e ) {
+  tabCompletion: function ( e ) {
+		var n, startIndex, word, value, c, lc,
+			list, i, listItem, listItemLC, beg,
+			end;
     util.stopEvent( e );
-    var n = e.srcElement;
+    n = e.srcElement;
     this.needsResetting = true;
     if ( this.nicks.length || this.channels.length ) {
       if ( this.tabFragment ) {
-        var startIndex = this.listItemIndex;
-        var word = this.tabFragment;
-        var value = this.savedValue;
-        var c = this.tabStart;
-        var lc = this.tabFragEnd;
+        startIndex = this.listItemIndex;
+        word = this.tabFragment;
+        value = this.savedValue;
+        c = this.tabStart;
+        lc = this.tabFragEnd;
       } else {
-        var c = 0;
-        var value = n.value;
-        var lc = n.selectionStart;
+        c = 0;
+        value = n.value;
+        lc = n.selectionStart;
         for ( c = ( lc - 1 ); c > 0; c-- ) {
-          if ( value[c] == " " ) break;
+          if ( value[c] === " " ) { break; }
         }
         this.tabStart = c;
         this.tabFragEnd = lc;
-        var startIndex = 0;
-        var word = util.trim( value.substring( c, lc ) ).toLowerCase( );
-        if ( !word ) return;
+        startIndex = 0;
+        word = util.trim( value.substring( c, lc ) ).toLowerCase( );
+        if ( !word ) { return; }
         this.tabFragment = word;
         this.savedValue = value;
       }
       word = word.split( "|" ).join( "\\|" ).split( "^" ).join( "\\^" ).split( "-" ).join( "\\-" );
       word = word.split( "[" ).join( "\\[" ).split( "]" ).join( "\\]" );
-      if ( word && word.length && word[ 0 ] == "#" ) {
-        var list = this.channels;
+      if ( word && word.length && word[ 0 ] === "#" ) {
+        list = this.channels;
       } else {
-        var list = this.nicks;
+        list = this.nicks;
       }
-      for ( var i = startIndex; i < list.length; i++ ) {
-        var listItem = list[ i ];
-        var listItemLC = listItem.toLowerCase( );
-        if ( listItemLC.search( word ) == 0 ) {
+      for ( i = startIndex; i < list.length; i++ ) {
+        listItem = list[ i ];
+        listItemLC = listItem.toLowerCase( );
+        if ( listItemLC.search( word ) === 0 ) {
           //add 
           this.listItemIndex = i + 1;
           //if c is 0 replace at beginning, if not, one char after c (that is after space)
-          var beg = value.slice( 0, ( c ? c + 1 : c ) );
-          var end = value.slice( lc, 0 );
+          beg = value.slice( 0, ( c ? c + 1 : c ) );
+          end = value.slice( lc, 0 );
           //if c is 0 add : and space, else add just a space
           this.setValue( [ beg, listItem, ( c ? " " : ": " ), end ].join( "" ) );
           //place cursor?
@@ -236,12 +250,13 @@ dojo.provide( "diom.view.formInput" );
       this.listItemIndex = 0;
       return;
     }
-  }
+  },
 
-  _vip.destroy = function ( ) {
+  destroy: function ( ) {
     delete this.nicks;
     delete this.history;
     delete this.input;
     delete this.form;
   }
 
+} );
