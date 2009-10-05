074967524bd8c21c9a71dadeb629add6a99219f2
Bjorn Tipling <bjorn@ambientchill.com>
2009-10-05 16:10:10 +0000

    activitywindow now uses dojo.create and passes jslint

--- js/diom/view/activityWindow.js
+++ js/diom/view/activityWindow.js
@@ -1,13 +1,21 @@
+/*jslint white: false */
+/*jslint nomen: false */
+/*jslint regexp: false */
+/*jslint plusplus: false */
+/*jslint passfail: true */
+/*global window, dojo, util, diom, air, document, alert */
 
 dojo.provide( "diom.view.activityWindow" );
 
-  dView.ActivityWindow = function ( serverName, channelName, maxItems, timeFormat ) {
+dojo.declare( "diom.view.ActivityWindow", null, {
+
+  constructor: function ( serverName, channelName, maxItems, timeFormat ) {
     this.linkRegex = /(https?:\/\/([\w\-]+\.)*[\w\-]+\.[\w\-]+(\/\S*)?)\s?/g;
-    this.colorCode = String.fromCharCode( 003 );
-    this.normalCode = String.fromCharCode( 017 );
-    this.boldCode = String.fromCharCode( 002 );
-    this.underlineCode = String.fromCharCode( 037 );
-    this.italicsCode = String.fromCharCode( 026 );
+    this.colorCode = String.fromCharCode( 3 );
+    this.normalCode = String.fromCharCode( 17 );
+    this.boldCode = String.fromCharCode( 2 );
+    this.underlineCode = String.fromCharCode( 37 );
+    this.italicsCode = String.fromCharCode( 26 );
     this.setTimeFormat( timeFormat );
     this.COLOR_CODES = [
       "#fff", //white
@@ -25,9 +33,9 @@ dojo.provide( "diom.view.activityWindow" );
       "#0066FF", //light blue
       "#BC8F8F", //pink
       "#C0C0C0", //grey
-      "#A8A8A8", //lightgrey
+      "#A8A8A8" //lightgrey
     ];
-    this.nickWindow = new dView.NickWindow( serverName, channelName );
+    this.nickWindow = new diom.view.NickWindow( serverName, channelName );
     this.topic = null;
     this.win = document.createElement( "div" );
     this.win.setAttribute( "class", "activityWin" );
@@ -39,82 +47,99 @@ dojo.provide( "diom.view.activityWindow" );
     util.subscribe( diom.topics.PREFS_CHANGE_HISTORY_LENGTH, this, "handleChangeHistoryLength", [] );
     util.subscribe( diom.topics.PREFS_CHANGE_TIME_FORMAT, this, "setTimeFormat", [] );
     util.subscribe( diom.topics.CHANNEL_TOPIC, this, "handleTopic", [] );
-  }
+  },
 
-  var _vap = dView.ActivityWindow.prototype;
+  sanitize: function ( msg ) {
+    if ( msg ) {
+      msg = msg.split( "&" ).join( "&amp;" );
+      msg = msg.split( "<" ).join( "&lt;" );
+      msg = msg.split( ">" ).join( "&gt;" );
+      msg = msg.split( '"' ).join( "&quot;" );
+    }
+    return msg;
+  },
 
-  _vap.sanitize = dView.View.prototype.sanitize;
+  setContents: function ( node, contents, synchronous ) {
+    if ( synchronous ) {
+      node.innerHTML = contents;
+    } else {
+      //per
+      window.setTimeout( function() { node.innerHTML = contents; }, 0 );
+    }
+  },
 
-  _vap.setContents = _vvp.setContents;
-
-  _vap.scrollUp = function ( ) {
+  scrollUp: function ( ) {
       this.win.scrollTop -= this.win.clientHeight;
-  }
+  },
 
-  _vap.scrollDown = function ( ) {
+  scrollDown: function ( ) {
       this.win.scrollTop += this.win.clientHeight;
-  }
+  },
 
-  _vap.getTopic = function ( ) {
+  getTopic: function ( ) {
     return this.topic;
-  }
+  },
 
-  _vap.handleTopic = function ( serverName, channelName, topic ) {
-    if ( serverName == this.serverName && channelName == this.channelName ) {
+  handleTopic: function ( serverName, channelName, topic ) {
+    if ( serverName === this.serverName && channelName === this.channelName ) {
       this.topic = topic;
     }
-  }
+  },
 
-  _vap.setTimeFormat = function ( timePrefs ) {
-    for ( var i = 0; i < timePrefs.length; i++ ) {
-      var time = timePrefs[ i ];
+  setTimeFormat: function ( timePrefs ) {
+		var i, time;
+    for ( i = 0; i < timePrefs.length; i++ ) {
+      time = timePrefs[ i ];
       if ( "selected" in time ) { 
         this.timeFormat = parseInt( time.value, 10 );
         return;
       }
     }
-  }
+  },
 
-  _vap.handleChangeHistoryLength = function ( newLen ) {
+  handleChangeHistoryLength: function ( newLen ) {
     this.maxItems = newLen;
     window.setTimeout( util.hitch( this, "shrinkActivity", [ newLen ] ), 0 );
-  }
+  },
 
-  _vap.shrinkActivity = function ( len ) {
-    var w = this.win;
-    for ( var i = this.win.childNodes.length; i > len; i-- ) {
+  shrinkActivity: function ( len ) {
+		var w, i;
+    w = this.win;
+    for ( i = this.win.childNodes.length; i > len; i-- ) {
       w.removeChild( w.firstChild );
     }
-  }
+  },
 
-  _vap.clear = function ( ) {
+  clear: function ( ) {
     this.win.innerHTML = "";
-  }
+  },
 
-  _vap.update = function ( messages, userNick, channelName ) {
+  update: function ( messages, userNick, channelName ) {
+		var w, diff, variance, isAtBottom, r, newMsg, showBrackets,
+			isServer, isAction, isNotice, referencesUser, m, isSelf,
+			dates, childNodes, n, msg, nick;
     //XXX: need to switch from using msg properties to using msg getters and setters
-    var w = this.win;
-    var diff = Math.abs( w.scrollTop - ( w.scrollHeight - w.offsetHeight ) );
-    var variance = 5;
-    var isAtBottom = false;
+    w = this.win;
+    diff = Math.abs( w.scrollTop - ( w.scrollHeight - w.offsetHeight ) );
+    variance = 5;
+    isAtBottom = false;
     if ( diff < variance ) {
       isAtBottom = true;
     }
-    if ( !messages || !messages.length) return;
-    var r = [], msg;
+    if ( !messages || !messages.length) { return; }
+    r = [];
     while ( messages.length ) {
       msg = messages.shift( );
-      var newMsg;
-      var showBrackets = msg.showBrackets( );
-      var isServer = msg.isServer( );
-      var isAction = msg.isAction( );
-      var isNotice = msg.isNotice( );
-      var referencesUser = msg.referencesUser( );
-      var m = msg.getMsg( );
-      if ( msg.isAction( ) ) showBrackets = false;
+      showBrackets = msg.showBrackets( );
+      isServer = msg.isServer( );
+      isAction = msg.isAction( );
+      isNotice = msg.isNotice( );
+      referencesUser = msg.referencesUser( );
+      m = msg.getMsg( );
+      if ( msg.isAction( ) ) { showBrackets = false; }
       nick = msg.getNickWithStatus( channelName );
-      var isSelf = ( msg.nick == userNick );
-      var dates = this.formatDate( msg.datetime );
+      isSelf = ( msg.nick === userNick );
+      dates = this.formatDate( msg.datetime );
       newMsg = [].concat( [
           '<div class="message">',
             '<span class="messageData">',
@@ -133,7 +158,7 @@ dojo.provide( "diom.view.activityWindow" );
             [
               ( showBrackets ? '&lt;' : ''  ),
               this.sanitize( nick ),
-              ( showBrackets ? '&gt;' : ''  ),
+              ( showBrackets ? '&gt;' : ''  )
             ].join( "" ),
             '</span>',
             '</span> ',
@@ -148,54 +173,55 @@ dojo.provide( "diom.view.activityWindow" );
             '</span> ',
           '</div>'
       ] );
-      var childNodes = w.childNodes;
+      childNodes = w.childNodes;
       if ( childNodes.length > this.maxItems ) {
         w.removeChild( w.firstChild );
       }
-      var n = document.createElement("div");
+      n = document.createElement("div");
       n.innerHTML = newMsg.join( "" );
       w.appendChild( n );
     }
     if ( isAtBottom ) {
       w.scrollTop = w.scrollHeight;
     }
-  }
+  },
 
-  _vap.changeView = function ( ) {
+  changeView: function ( ) {
     //scroll to bottom
     this.win.scrollTop = this.win.scrollHeight;
-  }
+  },
 
-  _vap.getNode = function ( ) {
+  getNode: function ( ) {
     return this.win;
-  }
+  },
 
-  _vap.formatDateItem = function ( dateItem ) {
+  formatDateItem: function ( dateItem ) {
     dateItem = dateItem.toString( );
-    if ( dateItem.length == 1 ) {
+    if ( dateItem.length === 1 ) {
       dateItem = [ "0",  dateItem ].join( "" );
     }
     return dateItem;
-  }
+  },
 
-  _vap.formatDate = function ( date_ ) {
-    var year = this.formatDateItem( date_.getFullYear( ) );
-    var month = this.formatDateItem( date_.getMonth( ) + 1 );
-    var day = this.formatDateItem( date_.getDate( ) );
-    var hour =  date_.getHours( );
+  formatDate: function ( date_ ) {
+		var year, month, day, hour, clock, minute, second;
+    year = this.formatDateItem( date_.getFullYear( ) );
+    month = this.formatDateItem( date_.getMonth( ) + 1 );
+    day = this.formatDateItem( date_.getDate( ) );
+    hour =  date_.getHours( );
     if ( this.timeFormat < 24 ) {
       if ( hour > 12 ) {
         hour = hour - 12;
-        var clock = "pm";
+        clock = "pm";
       } else {
-        var clock = "am";
+        clock = "am";
       }
     } else {
-      var clock = "";
+      clock = "";
     }
     hour = this.formatDateItem( hour );
-    var minute = this.formatDateItem( date_.getMinutes( ) );
-    var second = this.formatDateItem( date_.getSeconds( ) );
+    minute = this.formatDateItem( date_.getMinutes( ) );
+    second = this.formatDateItem( date_.getSeconds( ) );
     return {
       "long" : [
         "[",
@@ -222,9 +248,9 @@ dojo.provide( "diom.view.activityWindow" );
         "]"
       ].join( "" )
     };
-  }
+  },
 
-  _vap.textFormat = function ( msg, nick ) {
+  textFormat: function ( msg, nick ) {
     msg = msg.split("  ").join(" &nbsp;");
     msg = this.findBold( msg );
     msg = [ msg, this.closeOpenMarkup("isInBold", "</strong>" ) ].join( "" );
@@ -232,41 +258,44 @@ dojo.provide( "diom.view.activityWindow" );
     msg = [ msg, this.closeOpenMarkup("isInStyle", "</span>" ) ].join( "" );
     msg = this.findLinks( msg, nick );
     return msg;
-  }
+  },
 
-  _vap.findBold = function ( msg ) {
-    var pos = msg.search( this.boldCode );
-    if ( pos != -1 ) {
-      var beg = msg.slice( 0, pos );
+  findBold: function ( msg ) {
+		var pos, beg, middle, end, newMsg;
+    pos = msg.search( this.boldCode );
+    if ( pos !== -1 ) {
+      beg = msg.slice( 0, pos );
       if ( this.isInBold ) {
-        var middle = "</strong>";
+        middle = "</strong>";
         this.isInBold = false;
       } else {
-        var middle = "<strong>";
+        middle = "<strong>";
         this.isInBold = true;
       }
-      var end = msg.slice( pos + 1 );
-      var newMsg = [beg, middle, end].join( "" );
+      end = msg.slice( pos + 1 );
+      newMsg = [beg, middle, end].join( "" );
       newMsg = this.findBold( newMsg );
     } else {
-      var newMsg = msg;
+      newMsg = msg;
     }
     return newMsg;
-  }
+  },
   
-  _vap.findColors = function ( msg ) {
-    var pos = msg.search( this.colorCode );
-    var styles = [];
-    var styleLength = 0;
-    if ( pos != -1 ) {
+  findColors: function ( msg ) {
+		var pos, styles, styleLength, possibleCode, setResult,
+			commaPos, beg, middleParts, middle, end, newMsg;
+    pos = msg.search( this.colorCode );
+    styles = [];
+    styleLength = 0;
+    if ( pos !== -1 ) {
       styleLength += 1; //control character
-      var possibleCode = msg.substr( pos + 1, 7 );
-      var setResult = this.setColorStyle( possibleCode, false );
+      possibleCode = msg.substr( pos + 1, 7 );
+      setResult = this.setColorStyle( possibleCode, false );
       if ( setResult ) {
         styleLength += setResult.length;
         styles.push( setResult.style );
-        var commaPos = possibleCode.search( "," );
-        if ( commaPos != -1 ) {
+        commaPos = possibleCode.search( "," );
+        if ( commaPos !== -1 ) {
           possibleCode = possibleCode.substr( commaPos + 1);
           setResult = this.setColorStyle( possibleCode, true );
           if ( setResult ) {
@@ -275,8 +304,8 @@ dojo.provide( "diom.view.activityWindow" );
           }
         }
       }
-      var beg = msg.slice( 0, pos );
-      var middleParts = [];
+      beg = msg.slice( 0, pos );
+      middleParts = [];
       if ( this.isInStyle ) {
         middleParts.push( '</span>' );
         this.isInStyle = false;
@@ -288,55 +317,56 @@ dojo.provide( "diom.view.activityWindow" );
         this.isInStyle = true;
       }
       if ( middleParts.length ) {
-        var middle = middleParts.join( "" );
+        middle = middleParts.join( "" );
       } else {
-        var middle = "";
+        middle = "";
       }
-      var end = msg.slice( pos + styleLength );
-      var newMsg = [beg, middle, end].join( "" );
+      end = msg.slice( pos + styleLength );
+      newMsg = [beg, middle, end].join( "" );
       newMsg = this.findColors( newMsg );
     } else {
-      var newMsg = msg;
+      newMsg = msg;
     }
     return newMsg;
-  }
+  },
 
-  _vap.logLinks = function ( msg, nick ) {
-    var d = document.createElement( "div" );
+  logLinks: function ( msg, nick ) {
+		var d, anchors, i, a;
+    d = document.createElement( "div" );
     d.innerHTML = msg;
-    var anchors = d.getElementsByTagName( "a" );
+    anchors = d.getElementsByTagName( "a" );
     if ( anchors && anchors.length ) {
-      for ( var i = 0; i < anchors.length; i++ )  {
-        var a = anchors[ i ];
+      for ( i = 0; i < anchors.length; i++ )  {
+        a = anchors[ i ];
         util.publish( diom.topics.LINK_FOUND, [ a.getAttribute( "href" ), this.serverName, this.channelName, nick ] );
-        delete a;
+				a = null;
       }
     }
-    delete anchors;
-    delete d;
-  }
+		anchors = null;
+		d = null;
+  },
 
-  _vap.findLinks = function ( msg, nick ) {
+  findLinks: function ( msg, nick ) {
     var newMsg = msg.replace( this.linkRegex, ' <a target="_blank" class="ircLink" href="$1">$1</a> ' );
     if ( newMsg.split( "ircLink" ).length > 1 ) {
       this.logLinks( newMsg, nick );
     }
     return newMsg;
-  }
+  },
 
-  _vap.setColorStyle = function ( possibleCode, isBackground ) {
-    var color = parseInt( possibleCode, 10 );
-    var styleString;
-    var styleLength = 0;
+  setColorStyle: function ( possibleCode, isBackground ) {
+		var color, styleString, styleLength, styleName;
+    color = parseInt( possibleCode, 10 );
+    styleLength = 0;
     if ( isBackground ) {
-      var styleName = "background-color:";
+      styleName = "background-color:";
       styleLength += 1; //for comma
     } else {
-      var styleName = "color:";
+      styleName = "color:";
     }
     if ( !isNaN( color ) && color in this.COLOR_CODES ) {
       styleString = [ styleName , this.COLOR_CODES[ color ], ";" ].join( "" );
-      if ( possibleCode[ 0 ] == "0" || color > 9 ) {
+      if ( possibleCode[ 0 ] === "0" || color > 9 ) {
         styleLength += 2;
       } else {
         styleLength += 1;
@@ -345,9 +375,9 @@ dojo.provide( "diom.view.activityWindow" );
     } else {
       return null;
     }
-  }
+  },
 
-  _vap.closeOpenMarkup = function ( type_, markup) {
+  closeOpenMarkup: function ( type_, markup) {
       if ( this[ type_ ] ) {
         this[ type_ ] = false;
         return markup;
@@ -356,3 +386,4 @@ dojo.provide( "diom.view.activityWindow" );
       }
   }
 
+} );
