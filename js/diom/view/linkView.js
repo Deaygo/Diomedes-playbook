18a9e342fab00812062209784fea020c3023cc89
Bjorn Tipling <bjorn@ambientchill.com>
2009-10-05 15:52:10 +0000

    linkView using dojo.create and passes jslint

--- js/diom/view/linkView.js
+++ js/diom/view/linkView.js
@@ -1,22 +1,29 @@
+/*jslint white: false */
+/*jslint nomen: false */
+/*jslint regexp: false */
+/*jslint plusplus: false */
+/*jslint passfail: true */
+/*global window, dojo, util, diom, air, document, alert */
 
 dojo.provide( "diom.view.linkView" );
 
-  dView.LinkView = function ( node ) {
+dojo.declare( "diom.view.LinkView", null, {
+
+  constructor: function ( node ) {
     this.node = node;
     this.links = [];
     util.subscribe( diom.topics.LINK_DATA, this, "addLink", [] );
-  }
+  },
 
-  var _vlw = dView.LinkView.prototype;
-
-  _vlw.addLink = function ( link, properties ) {
+  addLink: function ( link, properties ) {
     util.log( "addLink" );
     this.links.push( this.createHTML( properties ) );
-  }
+  },
 
-  _vlw.createHTML = function ( p ) {
+  createHTML: function ( p ) {
+		var r, i, h;
     util.log( "createHTML" );
-    var r = [];
+    r = [];
     /*
         {
           "url": this.url,
@@ -34,9 +41,9 @@ dojo.provide( "diom.view.linkView" );
     r.push( '<div class="linkLogItem">' );
       r.push( '<h2>Link</h2>' );
       r.push( [ '<div> <strong>IRC Info:</strong>', p.serverName, "-", p.channelName, "-", p.nick, "</div>" ].join( " " ) );
-      r.push( [ '<div> <strong>Date:</strong>', p["date"], '</div>' ].join( " " ) );
+      r.push( [ '<div> <strong>Date:</strong>', p.date, '</div>' ].join( " " ) );
       r.push( [ '<div><strong>Given URL:</strong><a href="', p.url, '">', p.url, "</a></div>" ].join( " " ) );
-      if ( p.url != p.responseURL ) {
+      if ( p.url !== p.responseURL ) {
         r.push( [ '<div><strong>Response URL:</strong><a href="', p.responseURL, '">', p.responseURL, "</a></div>" ].join( " " ) );
       }
       if ( p.title ) {
@@ -45,23 +52,25 @@ dojo.provide( "diom.view.linkView" );
       r.push( '<div class="extraLinkInfoCon"><div class="extraLinkInfoConTitle">Show Additional Info</div><div class="extraLinkInfo">' );
         r.push( [ '<div><strong>HTTP Status:</strong>', p.httpStatus, '</div>' ].join( " " ) );
         r.push( '<div class="headers"><h3>Header Information for URL:</h3>' );
-        for ( var i = 0; i < p.headers.length; i++ ) {
-          var h = p.headers[ i ];
+        for ( i = 0; i < p.headers.length; i++ ) {
+          h = p.headers[ i ];
           r.push( [ '<div><strong>Name:</strong>', h.name, '<strong>Value:</strong>', ( h.value ? h.value : '' ), '</div>' ].join( " " ) );
         }
         r.push( '</div>' );
       r.push( "</div></div>" );
     r.push( "</div>" );
     return r.join( "" );
-  }
+  },
 
-  _vlw.display = function ( ) {
+  display: function ( ) {
+		var links;
     if ( this.links.length ) {
-      var links = this.links.slice( );
+      links = this.links.slice( );
       this.node.innerHTML = [ '<div id="linkLog"><h1>Link Log</h1>', links.reverse( ).join( " " ), "</div>" ].join( " " );
-      delete links;
+			links = null;
     } else {
       this.node.innerHTML = "No links found yet in IRC conversations. :/";
     }
   }
 
+} );
