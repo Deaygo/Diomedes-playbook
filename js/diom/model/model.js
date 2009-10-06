9722c58d57b32668299003a8cf84ea85e5642287
Bjorn Tipling <bjorn@ambientchill.com>
2009-10-06 01:19:20 +0000

    diom.model now uses dojo and passes jslint, this commit breaks trunk until I get all model files moved over to dojo/diom

--- js/diom/model/model.js
+++ js/diom/model/model.js
@@ -1,73 +1,76 @@
 /*
   Copyright (c) 2009 Apphacker apphacker@gmail.com
 */
+/*jslint white: false */
+/*jslint nomen: false */
+/*jslint regexp: false */
+/*jslint plusplus: false */
+/*jslint passfail: true */
+/*global window, dojo, util, diom, air, document, alert */
 
 dojo.provide( "diom.model.model" );
 
-var dModel;
+dojo.declare( "diom.view.Model", null, {
 
-if(!dModel) {
-  dModel = {};
-}
-
-if ( window.runtime && air && util ) {
-  //requires AIR and util
-
-  dModel.Model = function ( ) {
+  constructor: function ( ) {
     this.SQL_TYPES = {
       "INTEGER" : "INTEGER",
       "TEXT" : "TEXT",
       "BOOL" : "BOOLEAN",
       "PRIMARY_KEY" : "PRIMARY KEY",
-      "AUTOINCREMENT" : "AUTOINCREMENT",
+      "AUTOINCREMENT" : "AUTOINCREMENT"
     };
     this.statement = null;
     this.DATABASE_NAME = "diomedesModel.db";
-    this.prefs = new dModel.PrefModel( this );
-    this.aliases = new dModel.AliasModel( this );
-    this.networks = new dModel.NetworksModel( this );
-    this.ignores = new dModel.IgnoresModel( this );
+    this.prefs = new diom.model.PrefModel( this );
+    this.aliases = new diom.model.AliasModel( this );
+    this.networks = new diom.model.NetworksModel( this );
+    this.ignores = new diom.model.IgnoresModel( this );
     this.conn = null;
-  }
+  },
 
-  var _mmp = dModel.Model.prototype;
-
-  _mmp._openSQLConn = function ( type, errorHandler ) {
+  _openSQLConn: function ( type, errorHandler ) {
+		var dbFile;
     this.log( "Opening SQL connection..." );
     if ( !type ) {
       this.log( "Required params missing for _openSQLConn" );
       return;
     }
-    if ( !errorHandler ) errorHandler = util.hitch( this, "_handleError", [ "openSQLConnection" ] );
-    if ( this.conn ) this.closeConnection( );
+    if ( !errorHandler ) {
+			errorHandler = util.hitch( this, "_handleError", [ "openSQLConnection" ] );
+		}
+    if ( this.conn ) { this.closeConnection( ); }
     this.conn = new air.SQLConnection( ); 
-    var dbFile = air.File.applicationStorageDirectory.resolvePath( this.DATABASE_NAME ); 
+    dbFile = air.File.applicationStorageDirectory.resolvePath( this.DATABASE_NAME ); 
     try {
       this.conn.open( dbFile, type ); 
     } catch ( error ) {
       errorHandler( error );
     }
-  }
+  },
 
-  _mmp._executeSQL = function ( sql, type, resultsHandler, parameters, errorHandler, isRexecution ) {
+  _executeSQL: function ( sql, type, resultsHandler, parameters, errorHandler, isRexecution ) {
+		var s, i;
     this.log( "Executing SQL statement... sql: " + sql );
     if ( !sql || !type || !resultsHandler ) {
       this.log( "Required params missing for _executeSQL" );
       return;
     }
-    if ( !errorHandler ) errorHandler = util.hitch( this, "_handleError", [ "SQL: " + sql ] );
+    if ( !errorHandler ) {
+			errorHandler = util.hitch( this, "_handleError", [ "SQL: " + sql ] );
+		}
     if ( !this.conn || !this.conn.connected ) { 
       this.log( "Connection not open when calling _executeSQL, opening it");
       this._openSQLConn( type, errorHandler );
     }
-    var s = new air.SQLStatement( ); 
+    s = new air.SQLStatement( ); 
     s.sqlConnection = this.conn; 
     this.statement = s;
     this.log( "executing sql: " + sql );
     s.text = sql; 
     if ( parameters ) {
-      for ( var i in parameters ) {
-        if ( parameters.hasOwnProperty( i ) && i != "prototype" ) {
+      for ( i in parameters ) {
+        if ( parameters.hasOwnProperty( i ) && i !== "prototype" ) {
           s.parameters[ ":" + i ] = parameters[ i ];
         }
       }
@@ -78,56 +81,60 @@ if ( window.runtime && air && util ) {
       this._statementResultHandler( error, errorHandler );
     }
     this._statementResultHandler( null, resultsHandler );
-  }
+  },
 
-  _mmp._statementResultHandler = function ( e, resultsHandler ) {
+  _statementResultHandler: function ( e, resultsHandler ) {
+		var result;
     this.log( "Handling statement completion" );
-    var result = this.statement.getResult( );
+    result = this.statement.getResult( );
     this.log("deleting statement with sql of: " + this.statement.text );
     delete this.statement;
     this.statement = null;
     this.closeConnection( );
     resultsHandler( e, result );
-  }
+  },
 
-  _mmp._getResult = function ( e, result, resultsHandler ) {
+  _getResult: function ( e, result, resultsHandler ) {
     resultsHandler( result.data );
-  }
+  },
 
-  _mmp._getFilterResult = function ( e, results, resultsHandler ) {
+  _getFilterResult: function ( e, results, resultsHandler ) {
     //filters the event, and returns just the data;
     resultsHandler( results.data );
-  }
+  },
 
-  _mmp._getResultHandler = function ( resultsHandler ) {
+  _getResultHandler: function ( resultsHandler ) {
     return util.hitch( this, "_getFilterResult", [ resultsHandler ] );
-  }
+  },
 
-  _mmp._createTable = function ( name, types, resultsHandler, parameters, errorHandler ) {
+  _createTable: function ( name, types, resultsHandler, parameters, errorHandler ) {
+		var sql, typeNames;
     this.log("Creating Table.");
-    var sql = [];
+    sql = [];
     sql = sql.concat( [ "CREATE TABLE IF NOT EXISTS ", name, " (" ] );  
-    for ( var name in types ) {
-      sql.push( [ name, types[ name ] ].join( " " ) );
-      sql.push( ", ");
+    for ( typeNames in types ) {
+			if ( types.hasOwnProperty( name ) ) {
+				sql.push( [ name, types[ name ] ].join( " " ) );
+				sql.push( ", ");
+			}
     }
     sql.pop( ); //get rid of last comma
     sql.push( ")" );
     this._executeSQL( sql.join( "" ), air.SQLMode.CREATE, resultsHandler, parameters, errorHandler );
-  }
+  },
 
-  _mmp._handleError = function ( e, msg ) {
+  _handleError: function ( e, msg ) {
     this.log("error: " + e);
     this.log("message: " + msg );
     this.log("Error message:", e.error.message); 
     this.log("Details:", e.error.details); 
-  }
+  },
 
-  _mmp.log = function ( msg ) {
+  log: function ( msg ) {
     util.log("\nSQL LOG: " + msg );
-  }
+  },
 
-  _mmp.closeConnection = function ( ) {
+  closeConnection: function ( ) {
     this.log("close connection");
     if ( this.conn && this.conn.connected ) {
       this.conn.close( );
@@ -135,6 +142,6 @@ if ( window.runtime && air && util ) {
     this.conn = null;
   }
 
-}
+} );
 
 
