5a29fe4069a54b0cad27f052d16e15b89c2e6e9c
Bjorn Tipling <bjorn@ambientchill.com>
2009-10-06 03:20:55 +0000

    moved ignoresModel over to use dojo.create and passes jslint

--- js/diom/model/ignoresModel.js
+++ js/diom/model/ignoresModel.js
@@ -1,62 +1,73 @@
+/*jslint white: false */
+/*jslint nomen: false */
+/*jslint regexp: false */
+/*jslint plusplus: false */
+/*jslint passfail: true */
+/*global window, dojo, util, diom, air, document, alert, DOMParser, XMLSerializer */
 
 dojo.provide( "diom.model.ignoresModel" );
 
-  dModel.IgnoresModel = function ( model ) {
+dojo.declare( "diom.view.NetworkModel", null, {
+
+  constructor: function ( model ) {
     this.model = model;
     this.createTables( );
-  }
+  },
 
-  var _mip = dModel.IgnoresModel.prototype;
-
-  _mip.createTables = function ( ) {
-    var st = this.model.SQL_TYPES;
-    var types; 
+  createTables: function ( ) {
+		var st, types, tableName;
+    st = this.model.SQL_TYPES;
     types = {
       id : [ st.INTEGER, st.PRIMARY_KEY, st.AUTOINCREMENT ].join( " " ),
       regex : st.TEXT,
-      active : st.BOOL,
-    }
-    var tableName = "ignores";
+      active : st.BOOL
+    };
+    tableName = "ignores";
     this.model._createTable( tableName, types, util.hitch( this, "_handleCreateTable", [ tableName ], null ) );
-  }
+  },
 
-  _mip.getIgnores = function ( resultsHandler ) {
-    var sql = "SELECT * FROM ignores";
-    var p = {};
+  getIgnores: function ( resultsHandler ) {
+		var sql, p;
+    sql = "SELECT * FROM ignores";
+    p = {};
     this.model._executeSQL( sql, air.SQLMode.READ, this.model._getResultHandler( resultsHandler ), p ); 
-  }
+  },
 
-  _mip.addIgnore = function ( regex, active ) {
-    var sql = "INSERT INTO ignores ( regex, active ) VALUES ( :regex, :active )";
-    var p = {
+  addIgnore: function ( regex, active ) {
+		var sql, p;
+    sql = "INSERT INTO ignores ( regex, active ) VALUES ( :regex, :active )";
+    p = {
       regex : regex,
       active : active
-    }
+    };
     this.model._executeSQL( sql, air.SQLMode.UPDATE, util.hitch( this, "_handleChange" ), p ); 
-  }
+  },
 
-  _mip.editIgnore = function ( id, regex, active ) {
-    var sql = "UPDATE ignores SET regex = :regex, active = :active WHERE id = :id";
-    var p = {
+  editIgnore: function ( id, regex, active ) {
+		var sql, p;
+    sql = "UPDATE ignores SET regex = :regex, active = :active WHERE id = :id";
+    p = {
       id : id,
       regex : regex,
       active : active
-    }
+    };
     this.model._executeSQL( sql, air.SQLMode.UPDATE, util.hitch( this, "_handleChange" ), p ); 
-  }
+  },
 
-  _mip.remIgnore = function ( id ) {
-    var sql = "DELETE FROM ignores WHERE id = :id ";
-    var p = { id : id };
+  remIgnore: function ( id ) {
+		var sql, p;
+    sql = "DELETE FROM ignores WHERE id = :id ";
+    p = { id : id };
     this.model._executeSQL( sql, air.SQLMode.UPDATE, util.hitch( this, "_handleChange" ), p ); 
-  }
+  },
 
-  _mip._handleCreateTable = function ( e, tableName ) {
+  _handleCreateTable: function ( e, tableName ) {
     this.model.log( "Created NetworksModel table: "  + tableName );
-  }
+  },
 
-  _mip._handleChange = function ( e ) {
+  _handleChange: function ( e ) {
     util.log( "Database changed." );
     util.publish( diom.topics.IGNORES_CHANGE, [ null ] );
   }
 
+} );
