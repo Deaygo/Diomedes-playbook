39ef5757539666975ef034fe944fa6041ea2e398
Apphacker <apphacker@gmail.com>
2010-06-20 14:21:47 -0700

    Switching tabs to spaces in updater.js

--- js/diom/controller/updater.js
+++ js/diom/controller/updater.js
@@ -9,23 +9,23 @@ dojo.provide( "diom.controller.updater" );
 dojo.declare( "diom.controller.Updater", null, {
 
   constructor: function ( updateDelay, updateURL ) {
-		var file;
+    var file;
     this.updateURL = updateURL;
-		if ( air ) {
-			this.appUpdater = new runtime.air.update.ApplicationUpdaterUI( ); 
-			file = new air.File( "app:/updateConfig.xml" );
-		} else {
-			this.appUpdater = null;
-			file = null;
-		}
+    if ( air ) {
+      this.appUpdater = new runtime.air.update.ApplicationUpdaterUI( ); 
+      file = new air.File( "app:/updateConfig.xml" );
+    } else {
+      this.appUpdater = null;
+      file = null;
+    }
     this.didCheckNow = false;
-		if ( air ) {
-			this.appUpdater.configurationFile = file;
-			this.appUpdater.updateURL = this.updateURL;
-			this.appUpdater.delay = updateDelay;
-    	this.appUpdater.addEventListener( air.StatusUpdateEvent.UPDATE_STATUS, dojo.hitch( this, "onStatus" ) ); 
-    	this.appUpdater.initialize();
-		}
+    if ( air ) {
+      this.appUpdater.configurationFile = file;
+      this.appUpdater.updateURL = this.updateURL;
+      this.appUpdater.delay = updateDelay;
+      this.appUpdater.addEventListener( air.StatusUpdateEvent.UPDATE_STATUS, dojo.hitch( this, "onStatus" ) ); 
+      this.appUpdater.initialize();
+    }
     dojo.subscribe(  diom.topics.UPDATE_CHECK, this, "checkNow" );
     dojo.subscribe(  diom.topics.UPDATE_DELAY_CHANGE, this, "changeUpdateDelay" );
     dojo.subscribe(  diom.topics.UPDATE_URL_CHANGE, this, "changeUpdateURL" );
@@ -39,30 +39,30 @@ dojo.declare( "diom.controller.Updater", null, {
   },
 
   changeUpdateURL: function ( updateURL ) {
-		if ( this.appUpdater ) {
-			this.appUpdater.delay = updateURL;
-			this.appUpdater.initialize( );
-		}
+    if ( this.appUpdater ) {
+      this.appUpdater.delay = updateURL;
+      this.appUpdater.initialize( );
+    }
   },
 
   changeUpdateDelay: function ( updateDelay ) {
-		if ( this.appUpdater ) {
-			this.appUpdater.delay = updateDelay;
-			this.appUpdater.initialize( );
-		}
+    if ( this.appUpdater ) {
+      this.appUpdater.delay = updateDelay;
+      this.appUpdater.initialize( );
+    }
   },
 
   checkNow: function ( ) {
     this.didCheckNow = true;
-		if ( this.appUpdater ) {
-    	this.appUpdater.checkNow( );
-		}
+    if ( this.appUpdater ) {
+      this.appUpdater.checkNow( );
+    }
   },
 
   destroy: function ( ) {
-		if ( this.appUpdater ) {
-    	delete this.appUpdater;
-		}
+    if ( this.appUpdater ) {
+      delete this.appUpdater;
+    }
   }
 
 } );
